a894385b938138e35d38d05f7063b89ddcb96671
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-09-20 20:52:03 +0100

    Register with writer

--- src/main/java/net/ollie/protobuf/jaxrs/ProtobufCompatibleMessageBodyWriter.java
+++ src/main/java/net/ollie/protobuf/jaxrs/ProtobufCompatibleMessageBodyWriter.java
@@ -1,6 +1,6 @@
 package net.ollie.protobuf.jaxrs;
 
-import net.ollie.protobuf.WritesProto;
+import com.google.protobuf.Message;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -11,25 +11,40 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 import static net.ollie.protobuf.jaxrs.ProtobufMediaType.isProtobufType;
 
 /**
- * Writes any implementation of {@link WritesProto}.
+ * Writes any object type that has been {@link #register registered}.
  */
 @Provider
 @Produces(ProtobufMediaType.APPLICATION_PROTOBUF)
-public class ProtobufCompatibleMessageBodyWriter implements MessageBodyWriter<WritesProto> {
+public class ProtobufCompatibleMessageBodyWriter implements MessageBodyWriter<Object> {
 
-    @Override
-    public boolean isWriteable(final Class<?> aClass, final Type type, final Annotation[] annotations, final MediaType mediaType) {
-        return isProtobufType(mediaType)
-                && WritesProto.class.isAssignableFrom(aClass);
+    private final Map<Class<?>, WriteFunction<?>> writers = new HashMap<>();
+
+    public <T> void register(final Class<T> type, final WriteFunction<? super T> toProto) {
+        writers.put(type, toProto);
     }
 
     @Override
-    public void writeTo(final WritesProto writesProto, Class<?> aClass, final Type type, final Annotation[] annotations, final MediaType mediaType, final MultivaluedMap<String, Object> multivaluedMap, final OutputStream outputStream) throws IOException {
-        writesProto.writeTo(outputStream);
+    public boolean isWriteable(final Class<?> type, final Type genericType, final Annotation[] annotations, final MediaType mediaType) {
+        return isProtobufType(mediaType)
+                && writers.containsKey(type);
+    }
+
+    @Override
+    public void writeTo(final Object object, final Class<?> type, final Type genericType, final Annotation[] annotations, final MediaType mediaType, final MultivaluedMap<String, Object> multivaluedMap, final OutputStream outputStream) throws IOException {
+        final WriteFunction writer = writers.get(type);
+        writer.toProto(object).writeTo(outputStream);
+    }
+
+    public interface WriteFunction<T> {
+
+        Message toProto(T object);
+
     }
 
 }
