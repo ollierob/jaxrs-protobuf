03d5f3a905a3156a928ca22206c99f80ac43fd22
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-10-10 17:38:20 +0100

    Also write BuildsProto

--- src/main/java/net/ollie/protobuf/jaxrs/ProtobufCompatibleMessageBodyWriter.java
+++ src/main/java/net/ollie/protobuf/jaxrs/ProtobufCompatibleMessageBodyWriter.java
@@ -1,7 +1,9 @@
 package net.ollie.protobuf.jaxrs;
 
 import com.google.protobuf.Message;
+import net.ollie.protobuf.BuildsProto;
 
+import javax.annotation.Nonnull;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
@@ -13,6 +15,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import static net.ollie.protobuf.jaxrs.ProtobufMediaType.isProtobufType;
 
@@ -41,7 +44,7 @@ public class ProtobufCompatibleMessageBodyWriter implements MessageBodyWriter<Ob
     @Override
     public boolean isWriteable(final Class<?> type, final Type genericType, final Annotation[] annotations, final MediaType mediaType) {
         return (isProtobufType(mediaType) || this.isWriteableString(mediaType))
-                && writers.containsKey(type);
+                && (writers.containsKey(type) || BuildsProto.class.isAssignableFrom(type));
     }
 
     private boolean isWriteableString(final MediaType mediaType) {
@@ -50,10 +53,20 @@ public class ProtobufCompatibleMessageBodyWriter implements MessageBodyWriter<Ob
 
     @Override
     public void writeTo(final Object object, final Class<?> type, final Type genericType, final Annotation[] annotations, final MediaType mediaType, final MultivaluedMap<String, Object> multivaluedMap, final OutputStream outputStream) throws IOException {
-        final WriteFunction writer = writers.get(type);
+        final var writer = this.getWriter(type);
         final var proto = writer.toProto(object);
         if (this.isWriteableString(mediaType)) outputStream.write(proto.toString().getBytes());
         else proto.writeTo(outputStream);
+    }
+
+    @Nonnull
+    private WriteFunction getWriter(final Class<?> type) {
+        var writer = writers.get(type);
+        if (writer == null && BuildsProto.class.isAssignableFrom(type)) {
+            writer = (WriteFunction<BuildsProto>) BuildsProto::toProto;
+            writers.putIfAbsent(type, writer);
+        }
+        return Objects.requireNonNull(writer);
     }
 
     public interface WriteFunction<T> {
