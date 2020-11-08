9be6f8a1fd4c200c735a0aa441d5bd1cb4c83f55
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-11-08 21:29:13 +0000

    Timestamp proto helper

--- src/main/java/net/ollie/protobuf/WritesProto.java
+++ src/main/java/net/ollie/protobuf/WritesProto.java
@@ -1,10 +1,20 @@
 package net.ollie.protobuf;
 
+import com.google.protobuf.Timestamp;
+
 import java.io.IOException;
 import java.io.OutputStream;
+import java.time.Instant;
 
 public interface WritesProto {
 
     void writeTo(OutputStream out) throws IOException;
+
+    static Timestamp writeTimestamp(final Instant time) {
+        return Timestamp.newBuilder()
+                .setSeconds(time.getEpochSecond())
+                .setNanos(time.getNano())
+                .build();
+    }
 
 }
